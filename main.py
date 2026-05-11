d282c732851f1cf0522f2eb17576f5682afbc346
Munilal Tanti <munikabir2@gmail.com>
2026-05-11 15:01:42 +0530

    fixed users table

--- main.py
+++ main.py
@@ -6,30 +6,58 @@ from starlette.middleware.sessions import SessionMiddleware
 from sqlalchemy.orm import Session
 from dotenv import load_dotenv
 import os
+
 from auth import router as auth_router
-from database import get_db
-from models import User
+
+# database
+from database import get_db, engine
+
+# models
+from models import User, Base
+
 from templates import templates
 
 # Other routers
 from routers import (
     cart, products, seller, order, shop,
-    payment, webhook, seller_profile, user_profile, subscription
+    payment, webhook, seller_profile,
+    user_profile, subscription
 )
 
 load_dotenv(dotenv_path=".env", override=True)
 
 app = FastAPI(debug=True)
 
+# ==============================
+# CREATE DATABASE TABLES
+# ==============================
+Base.metadata.create_all(bind=engine)
+
+# ==============================
 # Middleware
-app.add_middleware(SessionMiddleware, secret_key="supersecretkey")
+# ==============================
+app.add_middleware(
+    SessionMiddleware,
+    secret_key="supersecretkey"
+)
 
+# ==============================
 # Templates
+# ==============================
 templates = Jinja2Templates(directory="templates")
-# Static files
-app.mount("/static", StaticFiles(directory="static"), name="static")
 
-# Include routers
+# ==============================
+# Static files
+# ==============================
+app.mount(
+    "/static",
+    StaticFiles(directory="static"),
+    name="static"
+)
+
+# ==============================
+# Routers
+# ==============================
 app.include_router(auth_router)
 app.include_router(products.router)
 app.include_router(cart.router)
@@ -42,11 +70,18 @@ app.include_router(seller_profile.router)
 app.include_router(user_profile.router)
 app.include_router(subscription.router)
 
-
+# ==============================
 # Home route
+# ==============================
 @app.get("/", response_class=HTMLResponse)
-def home(request: Request, db: Session = Depends(get_db)):
-    context = {"request": request}
+def home(
+    request: Request,
+    db: Session = Depends(get_db)
+):
+
+    context = {
+        "request": request
+    }
 
     user = db.query(User).first()
 
@@ -56,9 +91,23 @@ def home(request: Request, db: Session = Depends(get_db)):
             "name": user.name
         }
 
-    return templates.TemplateResponse("login.html", context)
+    return templates.TemplateResponse(
+        "login.html",
+        context
+    )
+
+# ==============================
 # Run local
+# ==============================
 if __name__ == "__main__":
+
     import uvicorn
+
     port = int(os.environ.get("PORT", 10000))
-    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
+
+    uvicorn.run(
+        "main:app",
+        host="0.0.0.0",
+        port=port,
+        reload=True
+    )
